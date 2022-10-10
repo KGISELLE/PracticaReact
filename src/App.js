2b648a6c5e2ae0db2401021c27d8e57c2f42ec0a
Giselle Bernal <kgiselle17@gmail.com>
2022-10-10 17:58:50 -0500

    Configuracion repo con create react app y maquetacion inicial tarjetas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
+import lago from './assets/img/lago.jpg';
+import mountain from './assets/img/mountain.jpg';
+import playa from './assets/img/playa.jpg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="logo-wrap">
+        <img className="logo-img" src={logo} alt=""></img>
+      </div>
+      <h1 id="text-title">Bienvenido!!!</h1>
+      <nav className="menu-wrap">
+        <ul>
+          <li>Home</li>
+          <li>Contacto</li>
+          <li>Acerca de</li>
+          <li>Servicios</li>
+          <li>Personal</li>
+        </ul>
+      </nav>
+      <section>
+        <div className="card-wrap">
+          <div className="card">
+            <div className='img-wrap'>
+            <img src={mountain}></img>
+            </div>
+            <h2>Montañas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div className="card">
+            <div className='img-wrap'>
+              <img src={lago}></img>
+            </div>
+            <h2>Lagos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div className="card">
+            <div className='img-wrap'>
+              <img src={playa}></img>
+            </div>
+            <h2>Playas</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
